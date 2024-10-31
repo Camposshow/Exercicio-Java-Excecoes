3ca2944706329eb05be9fd547422c9e7782b90d6
Gabriel Campos <gcamposlive@gmail.com>
2024-10-31 16:28:47 -0300

    Resolução

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+/*Fazer um programa para ler os dados de uma conta bancária e depois realizar um
+saque nesta conta bancária, mostrando o novo saldo. Um saque não pode ocorrer
+ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de
+saque da conta. Implemente a conta bancária conforme projeto abaixo:
+*/
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Digite sua conta");
+			System.out.print("Numero: ");
+			Integer number = sc.nextInt();
+			System.out.print("Titular: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Deposito inicial: ");
+			Double initialDeposit = sc.nextDouble();
+			System.out.print("Limite de saque: ");
+			Double withDrawLimit = sc.nextDouble();
+			
+			Account account = new Account(number, holder, initialDeposit, withDrawLimit);
+			
+			System.out.print("\nDigite quanto deseja sacar: ");
+			Double amount = sc.nextDouble();
+			account.withDraw(amount);
+			System.out.print("Novo saldo: " + account.getBalance());
+		}
+		catch(DomainException e){ // Catch da exceção personalizada criada no metodo withDraw
+			System.out.println("Erro no saque: " + e.getMessage());
+		}
+		catch(RuntimeException e){ // Catch caso tenha algum erro de digitação
+			System.out.println("Erro no saque");
+			
+		}
+		sc.close();
+	}
+}
